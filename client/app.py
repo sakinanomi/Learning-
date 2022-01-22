7b472c457a2a08dbf0dd87ced7afd35e395ce30d
sakinanomi <56465213+sakinanomi@users.noreply.github.com>
2022-01-22 09:16:46 +0530

    fucnt

--- client/app.py
+++ client/app.py
@@ -4,15 +4,22 @@ import requests
 app=Flask(__name__)
 
 
-
-@app.route('/')
+locations=requests.get('http://127.0.0.1:5000/get_location_names')
+locations=locations.json()['locations']
+pred_price=None
+@app.route('/',methods=['POST','GET'])
 def hello():
-    return render_template('app.html',pred_price=None)
+    global locations
+
+
+    return render_template('app.html',pred_price=None,locations=locations)
 
 @app.route('/send_data',methods=['GET','POST'])
 def send_data():
+    global locations
 
     if request.method=='POST':
+
         area=float(request.form.get('total_sqft'))
         bhk=int(request.form.get('bhk'))
         bath=int(request.form.get('bath'))
@@ -26,19 +33,19 @@ def send_data():
               'location' : location
             }
 
+        print(area,bhk,bath,balcony,location)
+
 
         pred_price= requests.post('http://127.0.0.1:5000/predict_home_price',data=data)
 
         final =str(pred_price.json()['estimated_price']) + " Lakh"
 
 
-
-
         print(pred_price.json()['estimated_price'])
-        return render_template('app.html',pred_price=final)
+        return render_template('app.html',pred_price=final,locations=locations)
 
     else:
-        return render_template('app.html',pred_price=None)
+        return render_template('app.html',pred_price=None,locations=locations)
 
 if __name__=='__main__':
 
